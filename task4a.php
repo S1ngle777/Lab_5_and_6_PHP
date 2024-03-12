5a219f7f8ca3b9d39821b801c71db1844c023779
S1ngle777 <128795707+S1ngle777@users.noreply.github.com>
2024-03-12 12:14:48 +0200

    last update

--- task4a.php
+++ task4a.php
@@ -25,23 +25,31 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Проверяем, что все поля заполнены
         if (!empty($_POST['login']) && !empty($_POST['password'])) {
-            // Зашифровываем пароль пользователя с помощью функции md5()
-            $password = md5($_POST['password']);
+            // Проверяем, существует ли уже такой пользователь
+            $login = $_POST['login'];
+            if(strpos(file_get_contents('users.txt'), $login) !== false){
+                echo "Пользователь с таким логином или паролем уже существует.";
+                exit;
+            } else {
+                // Зашифровываем пароль пользователя с помощью функции md5()
+                $password = md5($_POST['password']);
 
-            // Сохраняем данные пользователя в текстовый файл в формате: login:password
-            $user_data = $_POST['login'] . ':' . $password . "\n";
-            file_put_contents($file, $user_data, FILE_APPEND);
+                // Сохраняем данные пользователя в текстовый файл в формате: login:password
+                $user_data = $_POST['login'] . ':' . $password . "\n";
+                file_put_contents($file, $user_data, FILE_APPEND);
 
-            // Отправляем HTTP-код 201 (Created)
-            http_response_code(201);
-            echo "Регистрация прошла успешно! Вы будете перенаправлены через 3 секунды.";
+                // Отправляем HTTP-код 201 (Created)
+                http_response_code(201);
+                echo "Регистрация прошла успешно! Вы будете перенаправлены через 3 секунды.";
 
-            // Перенаправляем пользователя на task4b.php через 3 секунды
-            echo "<meta http-equiv='refresh' content='3;url=task4b.php'>";
+                // Перенаправляем пользователя на task4b.php через 3 секунды
+                echo "<meta http-equiv='refresh' content='3;url=task4b.php'>";
+            }
         } else {
             echo "Пожалуйста, заполните все поля.";
         }
     }
+
     ?>
 </body>
 
